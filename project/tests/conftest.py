5bca9467174fc13e5f955df232f8209032513d6d
Jonathan Plasse <jonathan.plasse@drotek.com>
2021-01-04 18:59:51 +0100

    RESTful Routes

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -2,8 +2,9 @@ import os
 
 import pytest
 from app.config import Settings, get_settings
-from app.main import create_application  # updated
+from app.main import create_application
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -13,9 +14,30 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    app = create_application()  # new
+    app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(app) as test_client:  # updated
+    with TestClient(app) as test_client:
+
+        # testing
+        yield test_client
+
+    # tear down
+
+
+# new
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
 
         # testing
         yield test_client
